9338ab11a229463cc0bdf49b3dcf098e17a67185
aniket <aniketitankar24@gmail.com>
2021-06-11 22:39:36 +0530

    Screenshot from any website task.

---
+++ screenshot_taker.py
@@ -0,0 +1,7 @@
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://www.google.com/')
+screenshot = driver.save_screenshot('my_screenshot.png')
